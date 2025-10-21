3194518959695d4a81ef18452ac2efe4a0e7e704
alex-gordon <alexander.loosli@stu.ocsb.ca>
2025-10-21 15:58:56 +0000

    added file i love computer science 100%git add -A

---
+++ main.ts
@@ -0,0 +1,17 @@
+/**
+ * @author mr loosli
+ * @version 1.0.0
+ * @date 2025-10-21
+ * @fileoverview This program practices with string literals.
+ */
+
+// string literal text
+console.log("I love computer science!!");
+
+// string literal text
+console.log("    I  love computer science!!");
+
+// string literal text 
+console.log("I   love   computer   science 100%");
+
+console.log("nDone");
