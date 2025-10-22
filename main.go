1138ad9f6a2ba900e5885b70f4221c086d53f455
alex-gordon <alexander.loosli@stu.ocsb.ca>
2025-10-22 16:06:02 +0000

    added file i love computer science 100%

---
+++ main.go
@@ -0,0 +1,27 @@
+/*
+ * Author: Mr Loosli
+ * Version 1.0.0
+ * Date 2025-10-22
+ * This program displays my name.
+*/
+
+package main
+
+import "fmt"
+
+func main () {
+  // In programming you oftin use // in front of text to tell
+	// the computer not to intercept or compile these lines.
+	// programmers call this comments.
+
+	//string literal text
+	fmt.Println("I love computer science!!")
+
+	//string literal text
+	fmt.Println("    I  love computer science 100%")
+
+	//string literal text
+	fmt.Println("I   love   computer   science   100%")
+
+	fmt.Println("\nDone")
+}
